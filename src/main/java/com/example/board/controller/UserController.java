1ec04cd935b7636efc1b1a0c5c5213777b1b07be
tjfwltn <tjfwltn1234@gmail.com>
2025-03-05 17:04:59 +0900

    user 생성 로직 작성

--- src/main/java/com/example/board/controller/UserController.java
+++ src/main/java/com/example/board/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.board.controller;
 
+import com.example.board.dto.UserRequest;
 import com.example.board.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +15,8 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/signup")
+    public String signup(@RequestBody UserRequest userRequest) {
+        return userService.create(userRequest);
+    }
 }
--- src/main/java/com/example/board/service/UserService.java
+++ src/main/java/com/example/board/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.board.service;
 
+import com.example.board.dto.UserRequest;
+import com.example.board.entity.User;
 import com.example.board.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +14,11 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public String create(UserRequest userRequest) {
+        User user = new User(userRequest.getUsername());
+        userRepository.save(user);
+        return "User created";
     }
 }
